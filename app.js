e458e00e603d74ae0881a54b7e1b542cfb9c4b2a
Dmitry_Grinchik <grinchikdmitry@gmail.com>
2026-06-11 10:04:23 +0200

    Add empty alpine.js + tv.js web-application

---
+++ app.js
@@ -0,0 +1,6 @@
+//Import all components
+// tv.js will only load what is needed
+
+//$tv.setConfig({ renderAll: false });
+$tv.setConfig({ waitForEveryone: false });
+// $tv.import({ define: 'site-datahandler', file: '/src/components/additionals/DataHandler'});
